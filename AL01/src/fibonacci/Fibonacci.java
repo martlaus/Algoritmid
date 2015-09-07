ca3d0e27f79e6f145717ff2cf4721bd2d851e65b
Mart Laus <mart.laus@ttu.ee>
2015-09-07 13:25:55 +0300

    rounding up

--- AL01/src/fibonacci/Fibonacci.java
+++ AL01/src/fibonacci/Fibonacci.java
@@ -9,12 +9,11 @@ import java.math.RoundingMode;
  * Created by mart on 7.09.15.
  */
 public class Fibonacci {
-    public static BigDecimal goldenRatio = BigDecimal.valueOf(1.6180339887498949025);
 
 
     public static void main(String[] args) {
-        System.out.println("Starting... " + find_index(5));
-        System.out.println("YAY" + find_index(10));
+        //System.out.println("Starting... " + find_index(5));
+        System.out.println("Anwser " + find_index(5));
 //        int n = 0;
 //        while(true) {
 //            System.out.println(fib(n));
@@ -40,26 +39,21 @@ public class Fibonacci {
     }
 
     public static int find_index(int precision) {
-        BigDecimal ratio = goldenRatio.setScale(precision, RoundingMode.HALF_UP);
-        System.out.println("Golden ratio: " + goldenRatio.setScale(precision, RoundingMode.HALF_UP));
+        BigDecimal goldenRatio = BigDecimal.valueOf(1.6180339887498949025);
+        BigDecimal ratio = goldenRatio.setScale(precision, RoundingMode.UP);
         int n = 0;
         while (true) {
-            BigDecimal result = null;
+            BigDecimal result;
             BigDecimal a = new BigDecimal(fib(n));
-            System.out.println("a " + a);
-
             BigDecimal b = new BigDecimal(fib(n-1));
-            System.out.println("b " + b);
-
 
             if(n > 2){
-                result = a.divide(b, precision, BigDecimal.ROUND_HALF_UP );
-                //result = new BigDecimal(fib(n).divide(fib(n-1)));
-                System.out.println("res " + result);
-                System.out.println("ratio " + ratio);
+                result = a.divide(b, precision, BigDecimal.ROUND_UP );
+                //System.out.println("res " + result);
+                //System.out.println("ratio " + ratio);
 
                 if (result.equals(ratio)) {
-                    System.out.println("Vastus " + n++);
+                    //System.out.println("Vastus " + ++n);
                     return n;
                 }
             }
