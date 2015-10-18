686ad969ff95f6120bc697c4e57f2f1fa0e6aa2e
Mart Laus <mart.laus@ttu.ee>
2015-10-18 19:44:13 +0300

    test

--- AL03/src/Main.java
+++ AL03/src/Main.java
@@ -1,31 +1,41 @@
-import dataStructure.Queue;
-import dataStructure.Stack;
+import dataStructure.Dequeue;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Starting...");
 
-        Stack stack = new Stack();
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        stack.push(40);
-        stack.push(50);
-        while (!stack.isEmpty()) {
-            Object value = stack.pop();
-            System.out.print(value);
-            System.out.print(" ");
-        }
-        System.out.println("\nQueue:");
+//        Stack stack = new Stack();
+//        stack.push(10);
+//        stack.push(20);
+//        stack.push(30);
+//        stack.push(40);
+//        stack.push(50);
+//        while (!stack.isEmpty()) {
+//            Object value = stack.pop();
+//            System.out.print(value);
+//            System.out.print(" ");
+//        }
+//        System.out.println("\nQueue:");
+//
+//        Queue queue = new Queue();
+//        queue.enqueue(10);
+//        queue.enqueue(20);
+//        queue.enqueue(30);
+//        while(!queue.isEmpty()) {
+//            System.out.println(queue.dequeue());
+//        }
 
-        Queue queue = new Queue();
-        queue.enqueue(10);
-        queue.enqueue(20);
-        queue.enqueue(30);
-        while(!queue.isEmpty()) {
-            System.out.println(queue.dequeue());
+        Dequeue dequeue = new Dequeue();
+        dequeue.pushFirst(10);
+        dequeue.pushFirst(20);
+        dequeue.pushFirst(30);
+        dequeue.pushFirst(40);
+
+        while (!dequeue.isEmpty()) {
+            System.out.println(dequeue.popFirst());
         }
+
 
     }
 }
--- AL03/src/dataStructure/Dequeue.java
+++ AL03/src/dataStructure/Dequeue.java
@@ -4,22 +4,42 @@ package dataStructure;
  * Created by mart on 18.10.15.
  */
 public class Dequeue {
+    Stack head;
+    Stack tail;
+
+
+    public Dequeue() {
+        head = new Stack();
+        tail = new Stack();
+    }
 
     public void pushFirst(Object object) {
+        head.push(object);
     }
 
     public void pushLast(Object object) {
+        tail.push(object);
     }
 
     public Object popFirst() {
-        return null;
+        if (head.isEmpty()) {
+            while (!tail.isEmpty()) {
+                head.push(tail.pop());
+            }
+        }
+        return head.pop();
     }
 
     public Object popLast() {
-        return null;
+        if (tail.isEmpty()) {
+            while (!head.isEmpty()) {
+                tail.push(head.pop());
+            }
+        }
+        return tail.pop();
     }
 
     public boolean isEmpty() {
-        return false;
+        return head.isEmpty() && tail.isEmpty();
     }
 }
