27ca141299aff2d527bb83e4b46dfa2bad7fb826
Mart Laus <mart.laus@ttu.ee>
2015-10-19 12:52:07 +0300

    update

--- HW01/src/dancers/Dancers.java
+++ HW01/src/dancers/Dancers.java
@@ -5,61 +5,51 @@ import java.util.List;
 
 public class Dancers implements IDancers {
 
-    //   public Tree avlTree = new Tree();
+    public Tree avlTree = new Tree();
 
 //    public Tree getAvlTree() {
 //        return avlTree;
 //    }
 
-    //    public void addDancer(MyDancerNode d) {
-//        avlTree.insert(d);
-//    }
-    public AVLTree avlTree = new AVLTree();
+    public void addDancer(MyDancerNode d) {
+        avlTree.insert(d);
+    }
 
     @Override
     public SimpleEntry<IDancer, IDancer> findPartnerFor(IDancer searcher) {
         if (searcher == null) {
             throw new IllegalArgumentException();
         }
+        MyDancerNode myDancerNode = new MyDancerNode(searcher.getID(), searcher.getHeight(), searcher.isMale());
+        myDancerNode.setOriginalIDancer(searcher);
 
-        MyNode partner = avlTree.insert(searcher);
-        SimpleEntry res = null;
-        if (partner != null) {
-            res = new SimpleEntry<>(searcher, partner.getOriginalIDancer());
-            avlTree.delete(partner);
+        if (searcher.isMale()) {
+            MyDancerNode woman = avlTree.searchAndRemoveWomen(myDancerNode.getHeight());
+            if (woman == null) {
+                avlTree.insert(myDancerNode);
+            } else {
+
+                return new SimpleEntry<>(myDancerNode, woman.getOriginalIDancer());
+            }
+
+        } else {
+            MyDancerNode male = avlTree.searchAndRemoveMen(searcher.getHeight());
+            if (male == null) {
+
+                avlTree.insert(myDancerNode);
+            } else {
+
+                return new SimpleEntry<>(myDancerNode, male.getOriginalIDancer());
+            }
         }
-        return res;
 
-
-//        MyDancerNode myDancerNode = new MyDancerNode(searcher.getID(), searcher.getHeight(), searcher.isMale());
-//        myDancerNode.setOriginalIDancer(searcher);
-//
-//        if (searcher.isMale()) {
-//            MyDancerNode woman = avlTree.searchAndRemoveWomen(myDancerNode.getHeight());
-//            if (woman == null) {
-//                avlTree.insert(myDancerNode);
-//            } else {
-//
-//                return new SimpleEntry<>(myDancerNode, woman.getOriginalIDancer());
-//            }
-//
-//        } else {
-//            MyDancerNode male = avlTree.searchAndRemoveMen(searcher.getHeight());
-//            if (male == null) {
-//
-//                avlTree.insert(myDancerNode);
-//            } else {
-//
-//                return new SimpleEntry<>(myDancerNode, male.getOriginalIDancer());
-//            }
-//        }
-//
-//        return null;
+        return null;
     }
 
     @Override
     public List<IDancer> returnWaitingList() {
         return avlTree.inorder();
+
     }
 
 //    @Override
